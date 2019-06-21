690767a9facb4a9130f1d12208e2dbb6ef700e88
makio135 <makio135@gmail.com>
2019-06-21 13:21:50 +0200

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.get('/', function(request, response) {
   // response.sendFile(__dirname + '/views/index.html');
-  response.json({ msg: 'yo' });
+  response.json({ msg: 'yo!' });
 });
 
 app.use(bodyParser.json());
